25fca5c284fb01ef1e8e671c5011cead6a32fa38
Nobori77 <minginew77@gmail.com>
2026-02-09 20:25:21 +0900

    Update CastingTask02.java

--- casting/src/castingTask/CastingTask02.java
+++ casting/src/castingTask/CastingTask02.java
@@ -4,19 +4,21 @@ public class CastingTask02 {
    public static void main(String[] args) {
 //      "8.43" + 2.59를 더해서 10이 나오도록 출력하기
 	   
-	   String data = "", result = "";
-	   double number = 0.0, dataResult = 0.0;
-	   int numberResult = 0;
-	   
-	   data = "8.43";
-	   number = 2.59;
-	   dataResult = Integer.parseInt("data");
-			   
-	   numberResult = (int)dataResult + (int)number;
-	   
-	   result = "(int)%d + (int)%d = %d";
-	   
-	   System.out.printf(result, dataResult, number, numberResult);
+		double num1 = 0.0, num2 = 0.0;
+		int num3 = 0, num4 = 0, numResult = 0;
+		String result = "";
+		
+		num1 = Double.parseDouble("8.43");
+		num2 = 2.59;
+		
+		num3 = (int)num1;
+		num4 = (int)num2;
+		
+		numResult = num3 + num4;
+		
+		result = "%d + %d = %d";
+		
+		System.out.printf(result, num3, num4, numResult);
 	   
 	   
    }
