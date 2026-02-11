959efcf31b4cd4ea9d8012b813520d5c5cff32af
Nobori77 <minginew77@gmail.com>
2026-02-11 20:33:24 +0900

    "feat: WhileTask02 Update"

--- control/src/whileTask/WhileTask02.java
+++ control/src/whileTask/WhileTask02.java
@@ -1,5 +1,7 @@
 package whileTask;
 
+import java.util.Scanner;
+
 // 사용자에게 무한 입력 상태로 
 // 입력 받은 모든 값의 합이 100 이상되면 프로그램이 종료된다.
 // 그리고 입력 받은 모든 값은 순서대로 출력한다
@@ -16,6 +18,24 @@ package whileTask;
 public class WhileTask02 {
    public static void main(String[] args) {
 	   
+		Scanner sc = new Scanner(System.in);
+		
+		String message = "숫자를 입력하세요";
+		int infintyNum = 0, total = 0;
+		
+		System.out.println(message);
+//		무한 입력 상태 만들기
+//		입력 받은 값 모두 더하는 상태 만들기
+		while(true) {
+			infintyNum = sc.nextInt();
+			
+			total += infintyNum;
+			
+			if (total >= 100) {
+				break;
+			}
+		}
+		System.out.println("프로그램을 종료합니다");	   
 	   
       
    }
